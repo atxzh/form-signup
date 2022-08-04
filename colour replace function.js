5c76cc7c7ee9b4045ccbfdfc58ed361dc93f8252
mlksathil <mahmoodathil12@gmail.com>
2022-08-04 14:06:30 +0530

    Update Coloring with Radio Buttons

--- colour replace function.js
+++
@@ -1,9 +0,0 @@
-let bg_clr = window.getComputedStyle(document.body).getPropertyValue('background-image');
-const bg_regex = new RegExp(/(\b\d{1,3})(?=%)/, 'g');
-let count = 0;
-
-const bg_percentage = [0, 80, 100]
-
-let bg_clr_new = bg_clr.replace(bg_regex, () => bg_percentage[count++]);
-
-console.log(bg_clr, bg_clr_new)
--- script.js
+++ script.js
@@ -65,51 +65,92 @@ elements.slice(2, elements.length).forEach((element, index) => {
 
 // !SECTION
 
+// SECTION Color Change
 const toggles = Array.from(document.querySelectorAll('.tog'))
 
 toggles.forEach((radio) => {
     radio.addEventListener('click', () => {
         radio.classList.add('select')
 
+        // Colors Gradient Percentage
         let bg_clr = window.getComputedStyle(document.body).getPropertyValue('background-image');
         const bg_regex = new RegExp(/(\b\d{1,3})(?=%)/, 'g');
-        let count = 0;
 
-        // ANCHOR DC
+        // Image
+        let img = document.querySelector('.image')
+        let bg_img = window.getComputedStyle(img).getPropertyValue('background-image')
+
+        const bgimg_regex = new RegExp(/rgb[a]?\(.+?\)|(?<=assets\/)([\w\.\_\-]+)/, 'g')
+
+        // ANCHOR Function to Change Based on Radio
+
+        function radioChange(bg_percentage, clr_light_full, clr_light_transp, clr_dark, bg_image, logo) {
+
+            // Color
+            document.documentElement.style.setProperty('--light-full', clr_light_full)
+            document.documentElement.style.setProperty('--light-transp', clr_light_transp)
+            document.documentElement.style.setProperty('--dark', clr_dark)
+
+            // Image
+            const bgimg_setup = [clr_light_full, clr_light_transp, clr_dark, bg_image]
+            let cnt = 0
+            let bg_img_new = bg_img.replace(bgimg_regex, () => bgimg_setup[cnt++])
+
+            img.style.setProperty('background-image', bg_img_new)
+
+            // Background Percentage
+            let count = 0;
+            let bg_clr_new = bg_clr.replace(bg_regex, () => bg_percentage[count++]);
+
+            document.body.style.setProperty('background-image', bg_clr_new)
+
+            // Logo
+            document.querySelector('#logo').setAttribute('src', logo)
+
+        }
+
         if (radio.value == 'dc') {
+            // ANCHOR DC
 
             const bg_percentage = [0, 80, 100]
+            const clr_light_full = 'var(--dc-light-full)'
+            const clr_light_transp = 'var(--dc-light-transp)'
+            const clr_dark = 'var(--dc-dark)'
+            const bg_image = 'DC_Bg.webp'
+            const logo = 'assets/DC_Logo.png'
 
-            let bg_clr_new = bg_clr.replace(bg_regex, () => bg_percentage[count++]);
+            radioChange(bg_percentage, clr_light_full, clr_light_transp, clr_dark, bg_image, logo)
 
-            document.body.style.setProperty('background-image', bg_clr_new)
-
-        }
-
-        // ANCHOR RAND
-        else if (radio.value == 'rand') {
+        } else if (radio.value == 'rand') {
+            // ANCHOR RAND
 
             const bg_percentage = [0, 30, 100]
+            const clr_light_full = 'var(--rand-light-full)'
+            const clr_light_transp = 'var(--rand-light-transp)'
+            const clr_dark = 'var(--rand-dark)'
+            const bg_image = 'Rand_Bg.jpg'
+            const logo = 'assets/Rand_Logo.png'
 
-            let bg_clr_new = bg_clr.replace(bg_regex, () => bg_percentage[count++]);
+            radioChange(bg_percentage, clr_light_full, clr_light_transp, clr_dark, bg_image, logo)
 
-            document.body.style.setProperty('background-image', bg_clr_new)
-
-        }
-
-        // ANCHOR MARVEL
-        else if (radio.value == 'marvel') {
+        } else if (radio.value == 'marvel') {
+            // ANCHOR MARVEL
 
             const bg_percentage = [0, 30, 50];
+            const clr_light_full = 'var(--marvel-light-full)'
+            const clr_light_transp = 'var(--marvel-light-transp)'
+            const clr_dark = 'var(--marvel-dark)'
+            const bg_image = 'Marvel_Bg.webp'
+            const logo = 'assets/Marvel_Logo.png'
 
-            let bg_clr_new = bg_clr.replace(bg_regex, () => bg_percentage[count++]);
-
-            document.body.style.setProperty('background-image', bg_clr_new)
+            radioChange(bg_percentage, clr_light_full, clr_light_transp, clr_dark, bg_image, logo)
 
         } else {
             return -1
         }
 
         toggles.filter((item) => { return item != radio }).forEach((item) => item.classList.remove('select'))
+
     })
 })
+// !SECTION
